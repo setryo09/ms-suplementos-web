92aa28b792503e34c048ca0cd50e5b11e4892850
setryo09 <sebas.lorru@gmail.com>
2026-08-31 18:03:59 -0300

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-// Funciones utilitarias compartidas por toda la web.
-
-function formatPrice(value) {
-  return "$" + Math.round(value).toLocaleString("es-AR");
-}
-
-function buildWhatsAppLink(number, message) {
-  return "https://wa.me/" + number + "?text=" + encodeURIComponent(message);
-}
